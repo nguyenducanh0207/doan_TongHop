d483379658ecc1edcef19369d99d2353d3c27a82
nguyenducanh0207 <anhnguyen02072000@gmail.com>
2022-05-25 14:56:02 +0700

    update

--- webBanHang/src/main/java/com/example/doan_tonghop/controller/ImageController.java
+++ webBanHang/src/main/java/com/example/doan_tonghop/controller/ImageController.java
@@ -6,31 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
 public class ImageController {
+    private static final Path CURRENT_FOLDER = Paths.get(System.getProperty("user.dir"));
+
     @Autowired
     private ImageService imageService;
     @PostMapping("upload")
     public ImageDTO uploadImage(@RequestParam("parentID") Long parentID,
                               @RequestParam("orderBy") Long orderBy,
-                              @RequestParam("file")MultipartFile file,
+                              @RequestParam("file")MultipartFile image,
                               @RequestParam("statusId") Long statusId,
-                              @RequestParam("imageType") Long imageType)
-    {
-        ImageDTO imageDTO = new ImageDTO();
-        String filename = StringUtils.cleanPath(file.getOriginalFilename());
-        imageDTO.setImageUrl(filename);
-        imageDTO.setParentId(parentID);
-        imageDTO.setOrderBy(orderBy);
-        imageDTO.setStatusId(statusId);
-        imageDTO.setImageType(imageType);
+                              @RequestParam("imageType") Long imageType) throws IOException {
+        Path staticPath = Paths.get("static");
+        Path imagePath = Paths.get("image");
+        if (!Files.exists(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath))) {
+            Files.createDirectories(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath));
+        }
+        Path file = CURRENT_FOLDER.resolve(staticPath).resolve(imagePath).resolve(image.getOriginalFilename());
+        try (OutputStream os = Files.newOutputStream(file)) {
+            os.write(image.getBytes());
+        }
+        {
+            ImageDTO imageDTO = new ImageDTO();
+            imageDTO.setImageUrl(imagePath.resolve(image.getOriginalFilename()).toString());
+            imageDTO.setParentId(parentID);
+            imageDTO.setOrderBy(orderBy);
+            imageDTO.setStatusId(statusId);
+            imageDTO.setImageType(imageType);
 
-        return imageService.save(imageDTO);
+            return imageService.save(imageDTO);
+        }
     }
 }
