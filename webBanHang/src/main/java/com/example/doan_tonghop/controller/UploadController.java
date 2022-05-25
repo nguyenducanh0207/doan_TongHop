e8a5b2383edb54de54aa62cb1bb4cd545245bbbf
nguyenducanh0207 <anhnguyen02072000@gmail.com>
2022-05-25 18:44:50 +0700

    update

--- webBanHang/src/main/java/com/example/doan_tonghop/controller/UploadController.java
+++ webBanHang/src/main/java/com/example/doan_tonghop/controller/UploadController.java
@@ -1,12 +1,15 @@
 package com.example.doan_tonghop.controller;
 
 import com.example.doan_tonghop.entity.ImageTableEntity;
-import com.example.doan_tonghop.model.ImageDTO;
+import com.example.doan_tonghop.entity.SaveImageEntity;
 import com.example.doan_tonghop.repository.ImageRepository;
+import com.example.doan_tonghop.repository.UploadsRepository;
 import com.example.doan_tonghop.service.impl.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,17 +20,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
-public class ImageController {
+public class UploadController {
     private static final Path CURRENT_FOLDER = Paths.get(System.getProperty("user.dir"));
 
     @Autowired
-    private ImageService imageService;
-    @Autowired
-    private ImageRepository imageRepository;
-    @PostMapping("/")
-    public ImageTableEntity uploadImage(@RequestParam("image")MultipartFile image) throws IOException {
+    private UploadsRepository uploadsRepository;
+
+    @PostMapping("uploads")
+    @ResponseStatus(HttpStatus.CREATED)
+    public SaveImageEntity create(@RequestParam MultipartFile image) throws IOException {
         Path staticPath = Paths.get("static");
-        Path imagePath = Paths.get("images");
+        Path imagePath = Paths.get("image");
         if (!Files.exists(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath))) {
             Files.createDirectories(CURRENT_FOLDER.resolve(staticPath).resolve(imagePath));
         }
@@ -36,9 +39,8 @@ public class ImageController {
         try (OutputStream os = Files.newOutputStream(file)) {
             os.write(image.getBytes());
         }
-
-            ImageTableEntity imageTableEntity = new ImageTableEntity();
-            imageTableEntity.setImageUrl(imagePath.resolve(image.getOriginalFilename()).toString());
-            return imageRepository.save(imageTableEntity);
+        SaveImageEntity saveImageEntity = new SaveImageEntity();
+        saveImageEntity.setImageUrl(imagePath.resolve(image.getOriginalFilename()).toString());
+        return uploadsRepository.save(saveImageEntity);
     }
 }
